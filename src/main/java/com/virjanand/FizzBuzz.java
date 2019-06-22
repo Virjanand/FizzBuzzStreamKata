e1d3d89ab7cb2b8fdb5373f3bee09e1f76141a89
Virjanand <virjanand@virjanand.com>
2019-06-22 09:54:32 +0200

    15 -> list with 3 replaced with FIZZ

--- src/main/java/com/virjanand/FizzBuzz.java
+++ src/main/java/com/virjanand/FizzBuzz.java
@@ -8,6 +8,7 @@ public class FizzBuzz {
     public List<String> getNumbers(int maxNumber) {
         return IntStream.rangeClosed(1, maxNumber)
                 .mapToObj(String::valueOf)
+                .map(number -> number.equals("3") ? "FIZZ" : number)
                 .collect(Collectors.toList());
     }
 }
